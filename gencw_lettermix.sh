3f797dfb5ab85b480b820d02352087aaa69c0f31
Heiko Voigt <hvoigt@hvoigt.net>
2017-10-02 18:42:48 +0200

    skip adding a message seperator at end of audio files
    ebook2cw automatically add a message seperator at the end. Lets skip
    that because otherwise the user might type that in the textbox which
    would then be counted as a mistake.

--- gencw_lettermix.sh
+++ gencw_lettermix.sh
@@ -15,6 +15,6 @@ fi
 for i in 00 01 02 03 04 05 06 07 08 09 10
 do
     ./lettermix "$characters" >lettermix_$i.txt
-    cat lettermix_$i.txt | ebook2cw -e 10 -w 14 -f 800 \
+    cat lettermix_$i.txt | ebook2cw -p -e 10 -w 14 -f 800 \
 	-o lettermix_${i} -t "DH3IKO CW Lettermix $i"
 done
