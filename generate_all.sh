870045dbd499914949e8706ef62bdc7d030eee70
Heiko Voigt <hvoigt@hvoigt.net>
2017-11-23 00:36:18 +0100

    allow enabling of german texts or english qso wordmixes via option
    We can now pass
    
      -t for generating texts from a german wordlist
    
    or
    
      -q for generating qso texts from american example qsos

--- generate_all.sh
+++ generate_all.sh
@@ -7,6 +7,8 @@
 my_path="$(dirname "$0")"
 
 lettermix_options=
+generate_texts=
+generate_qsomix=
 while test $# -gt 2
 do
     case "$1" in
@@ -34,6 +36,14 @@ do
         fi
         shift 2
         ;;
+    -t)
+        generate_texts=t
+        shift
+        ;;
+    -q)
+        generate_qsomix=t
+        shift
+        ;;
     *)
         break
     esac
@@ -50,21 +60,34 @@ outdir="$2"
 (cd "$my_path" &&
  ./gencw_lettermix.sh $lettermix_options "$characters" &&
  mkdir -p 1_letter && mv *_*.txt *_*.mp3 1_letter/ &&
-# ./gencw_qsomix.sh "$characters" &&
-# mkdir -p 2_qso && mv *_*.txt *_*.mp3 2_qso/ &&
-# ./gencw_texts.sh "$characters" &&
-# mkdir -p 3_text && mv *_*.txt *_*.mp3 3_text/ &&
+ if [ "$generate_qsomix" ]; then
+     ./gencw_qsomix.sh "$characters" &&
+     mkdir -p 2_qso && mv *_*.txt *_*.mp3 2_qso/
+ fi &&
+ if [ "$generate_texts" ]; then
+     ./gencw_texts.sh "$characters" &&
+     mkdir -p 3_text && mv *_*.txt *_*.mp3 3_text/
+ fi &&
  echo "#Letters" >0_Exercises.txt &&
  find 1_letter -name "*.txt" | sort | cut -f1 -d. >>0_Exercises.txt
-# echo "#QSOs" >>0_Exercises.txt &&
-# find 2_qso -name "*.txt" | sort | cut -f1 -d. >>0_Exercises.txt &&
-# echo "#Texts" >>0_Exercises.txt &&
-# find 3_text -name "*.txt" | sort | cut -f1 -d. >>0_Exercises.txt
+ if [ "$generate_qsomix" ]; then
+     echo "#QSOs" >>0_Exercises.txt &&
+     find 2_qso -name "*.txt" | sort | cut -f1 -d. >>0_Exercises.txt
+ fi &&
+ if [ "$generate_texts" ]; then
+     echo "#Texts" >>0_Exercises.txt &&
+     find 3_text -name "*.txt" | sort | cut -f1 -d. >>0_Exercises.txt
+ fi
 ) &&
 mkdir -p "$outdir" &&
 rm -rf "$outdir/1_letter" "$outdir/2_qso" "$outdir/3_text" &&
-#mv "$my_path/1_letter" "$my_path/2_qso" "$my_path/3_text" "$outdir/" &&
 mv "$my_path/1_letter" "$outdir/" &&
+if [ "$generate_qsomix" ]; then
+    mv "$my_path/2_qso" "$outdir/"
+fi &&
+if [ "$generate_texts" ]; then
+    mv "$my_path/3_text" "$outdir/"
+fi &&
 echo "$characters" >"$outdir/0_Letters.txt" &&
 mv "$my_path/0_Exercises.txt" "$outdir/" &&
 cp -r "$my_path/html/"* "$outdir/"
